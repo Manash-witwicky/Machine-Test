6c6a21a467125bd98796e55db4b41183595217d4
Manash Jyoti Das <manash.jyoti.das@syncron.com>
2020-01-13 01:33:56 +0530

    Machine test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
 })
-export class AppComponent {
-  title = 'machineTest';
+export class AppComponent implements OnInit {
+  constructor(private _http: HttpClient) {}
+
+  public result: any[];
+  public filteredData: any[];
+
+  public filterSearch(event) {
+    const { value = '' } = event;
+    this.filteredData = this.result.filter(cv => cv.email.includes(value));
+  }
+
+  ngOnInit() {
+    this._http
+      .get('https://reqres.in/api/users?page=2')
+      .subscribe((val: any) => {
+        this.result = val.data;
+        this.filteredData = this.result;
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {
+  MatExpansionModule,
+  MatFormFieldModule,
+  MatInputModule
+} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatExpansionModule,
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
